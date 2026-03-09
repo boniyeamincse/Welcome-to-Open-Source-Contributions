ae0c30eddeea4ee390c9109cd33806e0d7a6f2b6
anisurrahmanfarazi17 <anisurrahmanfarazi@gmail.com>
2026-03-09 21:57:13 +0600

    Add: Implement sum calculator solution in C

---
+++ challenges/beginner/02-sum-calculator/anisurrahmanfarazi17.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int sum(int a,int b);
+
+int main(){
+    printf("Sum: %d \n",sum(3,10));
+    printf("Sum: %d \n",sum(42,8));
+    printf("Sum: %d \n",sum(85,1));
+    printf("Sum: %d \n",sum(9,2));
+
+    return 0;
+}
+int sum(int x,int y){
+    return x+y;
+}
+
+
+// Name: Anisur Rahman Farazi
+    //  date: 09/03/2026
